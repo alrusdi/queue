be0f493747db674405ba58cca74838dfef1b8f05
alrusdi <alrusdi@gmail.com>
2012-09-12 23:27:59 +0600

    Основа вьюшек

--- app/queue/views.py
+++ app/queue/views.py
@@ -1,9 +1,52 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.template import Context, loader
+from queue.models import Company, MenuItem
+from django.shortcuts import redirect
 
-def index(request):
+def _get_company(company_id = False):
+    if company_id:
+        company = Company.objects.get(pk= company_id)
+    else:
+        company = Company.objects.get(subdomain = 'лефф')
+    return company
+
+def companies(request, company_id = False):
     t = loader.get_template('index.html')
+    current_company = _get_company(company_id)
+    if current_company.get_descendant_count() < 1:
+        return redirect('/services/'+company_id)
     c = Context({
+                'company':current_company
+                })
+    return HttpResponse(t.render(c))
+
+def services(request, company_id, service_id=False):
+    t = loader.get_template('index.html')
+    current_company = _get_company(company_id)
+    if service_id:
+        services = MenuItem.objects.get(pk=service_id).get_descendants()
+    else:
+        services = current_company.menuitem_set.all()
+
+    if service_id and len(services) < 1:
+        return redirect('/apply/'+company_id+'/'+service_id)
+
+    c = Context({
+                'company': current_company,
+                'services': list(services)
+                })
+    return HttpResponse(t.render(c))
+
+def apply(request, company_id, service_id):
+    t = loader.get_template('index.html')
+    company = Company.objects.get(pk = company_id)
+    service = MenuItem.objects.get(pk = service_id)
+
+
+    c = Context({
+                'company': company,
+                'services': [service],
+                'params': service.menuitemattribute_set.all()
                 })
     return HttpResponse(t.render(c))
--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', 'queue.views.index'),
+    (r'^company/(?P<company_id>.*)$', 'queue.views.companies'),
+    (r'^services/(?P<company_id>[0-9]+)/(?P<service_id>.*)$', 'queue.views.services'),
+    (r'^apply/(?P<company_id>[0-9]+)/(?P<service_id>[0-9]+)$', 'queue.views.apply'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
